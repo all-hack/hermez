82b8672f5ca8b904d78dab5c20dea1d8a067c112
all-hack <obelanger@fordham.edu>
2017-08-23 10:29:04 -0700

    mailer should work now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,10 +57,27 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  # ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.42.us.org',
+    port:                 25,
+    authentication:       :plain,
+    enable_starttls_auto: false,
+    
+  }  
+
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "hermez_#{Rails.env}"
   config.action_mailer.perform_caching = false
+
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
